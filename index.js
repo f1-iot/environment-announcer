16f73b0f1011d1905bc7aad39c2bc97368780ce0
Nicolas Galler <ngaller@gmail.com>
2018-02-25 17:18:43 -0500

    Initial: watching for events

---
+++ index.js
@@ -0,0 +1,39 @@
+const Particle = require('particle-api-js');
+const dotenv = require('dotenv');
+const particle = new Particle();
+
+dotenv.config();
+
+particle
+  .login({
+    username: process.env.PARTICLE_USERNAME,
+    password: process.env.PARTICLE_PASSWORD
+  })
+  .then(
+    function(data) {
+      const token = data.body.access_token;
+      watchForEvents(token);
+    },
+    function(err) {
+      console.log('Could not log in.', err);
+    }
+  );
+
+function watchForEvents(token) {
+  particle
+    .getEventStream({
+      auth: token,
+      name: 'environment',
+      deviceId: 'mine'
+    })
+    .then(stream => {
+      stream.on('event', data => {
+        const { tf } = JSON.parse(data.data);
+        outputTemperature(tf);
+      });
+    });
+}
+
+function outputTemperature(t) {
+  console.log('Temperature', t)
+}
